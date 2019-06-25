ab1c557746ecc35cebca478787fe200db33df3f1
DNHewavitharana <dilshan.15@cse.mrt.ac.lk>
2019-06-25 14:42:17 +0530

    XGBoost Algorithm

--- src/data_visualizer.py
+++ src/data_visualizer.py
@@ -4,9 +4,24 @@ from __future__ import division
 import pandas as pd
 import numpy as np
 import statsmodels.api as sm
+from sklearn.ensemble import GradientBoostingClassifier
 from statsmodels.tools import eval_measures
 import statsmodels.formula.api as smf
-from sklearn import preprocessing
+from sklearn.preprocessing import RobustScaler
+
+
+from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+import xgboost as xgb
+
+# First XGBoost model for Pima Indians dataset
+from numpy import loadtxt
+from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
 
 
 def preprocess_data(data_path, labels_path=None):
@@ -19,61 +34,35 @@ def preprocess_data(data_path, labels_path=None):
                 'station_min_temp_c']
 
     dataset = train_features[features]
+
     dataset = dataset.interpolate(method='linear')
     dataset = process_temp_features(dataset)
 
-    # scaling the dataset
-    mm_scaler = preprocessing.MinMaxScaler()
-    _train_minmax = mm_scaler.fit_transform(dataset)
+    # Trailing Moving Average
+    # Tail-rolling average transform
+    rolling = dataset.rolling(window=3, min_periods=1)
+    rolling_mean = rolling.mean()
 
-    scaled_ds = pd.DataFrame(data=_train_minmax[0:, 0:],
+    #standarization
+    transformer = RobustScaler().fit(rolling_mean)
+    standarize_ds = pd.DataFrame(data=transformer.transform(rolling_mean)[0:, 0:],
                           index=dataset.index,
                           columns=features)
 
     if (labels_path):
         train_labels = pd.read_csv(labels_path,
                                    index_col=[0, 1, 2])
-        scaled_ds = scaled_ds.join(train_labels)
-    sj= scaled_ds.loc['sj']
-    iq = scaled_ds.loc['iq']
+        standarize_ds = standarize_ds.join(train_labels)
+    sj= standarize_ds.loc['sj']
+    iq = standarize_ds.loc['iq']
     return sj, iq
+
 
 def process_temp_features(dataset):
     for col_name in dataset.columns:
         if col_name.endswith('temp_k'):
             dataset[col_name] = dataset[col_name] - 273.15
     return dataset
-
-
-data_path = '/home/karu/PycharmProjects/DengAI/data/dengue_features_train.csv'
-label_path = '/home/karu/PycharmProjects/DengAI/data/dengue_labels_train.csv'
-
-sj_train, iq_train = preprocess_data(data_path, label_path)
-
-
-# def fix_col_outliers(column, fence_low, fence_high, win_size=5):
-#
-#
-# def clean_training_data(dataset):
-#     for col_name in dataset.columns.values:
-#         q1 = dataset[col_name].quantile(0.25)
-#         q3 = dataset[col_name].quantile(0.75)
-#         iqr = q3 - q1  # Interquartile range
-#         fence_low = q1 - 1.5 * iqr
-#         fence_high = q3 + 1.5 * iqr
-#         dataset[col_name] = fix_col_outliers(dataset[col_name], fence_low, fence_high)
-#     return dataset
-
-# sj_train, iq_train = clean_train_data(sj_train), clean_train_data(iq_train)
-
-print(sj_train.shape)
-print(iq_train.shape)
-
-sj_train_subtrain = sj_train.head(700)
-sj_train_subtest = sj_train.tail(sj_train.shape[0] - 700)
-
-iq_train_subtrain = iq_train.head(400)
-iq_train_subtest = iq_train.tail(iq_train.shape[0] - 400)
 
 
 def get_best_model(train, test):
@@ -85,8 +74,6 @@ def get_best_model(train, test):
                     "station_avg_temp_c"
 
     grid = 10 ** np.arange(-8, -3, dtype=np.float64)
-
-    print(len(grid))
 
     best_alpha = []
     best_score = 1000
@@ -118,17 +105,82 @@ def get_best_model(train, test):
     return fitted_model
 
 
-sj_best_model = get_best_model(sj_train_subtrain, sj_train_subtest)
-iq_best_model = get_best_model(iq_train_subtrain, iq_train_subtest)
+def xg_boost_Algorithm_model(X_train, y_train, X_test, y_test):
+
+    # gbm = xgb.XGBClassifier(max_depth=3, n_estimators=300, learning_rate=0.05).fit(X_train, y_train)
+    # y_pred = gbm.predict(X_test)
+
+    # fit model no training data
+    model = XGBClassifier()
+    model.fit(X_train, y_train)
+    # make predictions for test data
+    y_pred = model.predict(X_test)
+    predictions = [round(value) for value in y_pred]
+    # evaluate predictions
+    accuracy = accuracy_score(y_test, predictions)
+    print("Accuracy: %.2f%%" % (accuracy * 100.0))
+    return model
+
+def gradient_boosting_algorithm(X_train, y_train, X_test, y_test):
+    learning_rate = 0.1
+
+    gb = GradientBoostingClassifier(loss="deviance", n_estimators=500, learning_rate= learning_rate, max_features="log2", max_depth=3,
+                                    random_state=0)
+    gb.fit(X_train, y_train)
+    print("Learning rate: ", learning_rate)
+    print("Accuracy score (training): {0:.3f}".format(gb.score(X_train, y_train)))
+    print("Accuracy score (validation): {0:.3f}".format(gb.score(X_test, y_test)))
+    return gb;
 
 
-sj_test, iq_test = preprocess_data('/home/karu/PycharmProjects/DengAI/data/dengue_features_test.csv')
+data_path = 'C:/Users/Dilshan/Documents/DengAI/data/dengue_features_train.csv'
+label_path = 'C:/Users/Dilshan/Documents/DengAI/data/dengue_labels_train.csv'
 
-sj_predictions = sj_best_model.predict(sj_test).astype(int)
-iq_predictions = iq_best_model.predict(iq_test).astype(int)
+sj_train, iq_train = preprocess_data(data_path, label_path)
 
-submission = pd.read_csv("/home/karu/PycharmProjects/DengAI/data/submission_format.csv",
+sj_train_subtrain = sj_train.head(700).as_matrix()
+sj_train_subtest = sj_train.tail(sj_train.shape[0] - 700).as_matrix()
+
+
+sj_train_subtrain_X = sj_train_subtrain[:,0:4]
+sj_train_subtrain_Y = sj_train_subtrain[:,4]
+
+sj_train_subtest_X = sj_train_subtest[:,0:4]
+sj_train_subtest_Y = sj_train_subtest[:,4]
+
+sj_best_model = xg_boost_Algorithm_model(sj_train_subtrain_X, sj_train_subtrain_Y, sj_train_subtest_X, sj_train_subtest_Y)
+
+sj_best_model_gradient = gradient_boosting_algorithm(sj_train_subtrain_X, sj_train_subtrain_Y, sj_train_subtest_X, sj_train_subtest_Y)
+
+iq_train_subtrain = iq_train.head(400).as_matrix()
+iq_train_subtest = iq_train.tail(iq_train.shape[0] - 400).as_matrix()
+
+
+iq_train_subtrain_X = iq_train_subtrain[:,0:4]
+iq_train_subtrain_Y = iq_train_subtrain[:,4]
+
+iq_train_subtest_X = iq_train_subtest[:,0:4]
+iq_train_subtest_Y = iq_train_subtest[:,4]
+
+
+iq_best_model = xg_boost_Algorithm_model(iq_train_subtrain_X, iq_train_subtrain_Y, iq_train_subtest_X, iq_train_subtest_Y)
+iq_best_model_gradient = gradient_boosting_algorithm(iq_train_subtrain_X, iq_train_subtrain_Y, iq_train_subtest_X, iq_train_subtest_Y)
+
+sj_test, iq_test = preprocess_data('C:/Users/Dilshan/Documents/DengAI/data/dengue_features_test.csv')
+
+sj_predictions = sj_best_model.predict(sj_test.as_matrix()).astype(int)
+iq_predictions = iq_best_model.predict(iq_test.as_matrix()).astype(int)
+
+sj_predictions_gradient = sj_best_model_gradient.predict(sj_test.as_matrix()).astype(int)
+iq_predictions_gradient = iq_best_model_gradient.predict(iq_test.as_matrix()).astype(int)
+
+
+submission = pd.read_csv("C:/Users/Dilshan/Documents/DengAI/data/submission_format.csv",
                          index_col=[0, 1, 2])
 
 submission.total_cases = np.concatenate([sj_predictions, iq_predictions])
-submission.to_csv("/home/karu/PycharmProjects/DengAI/data/benchmark.csv")
+submission.to_csv("C:/Users/Dilshan/Documents/DengAI/data/benchmark_xg_boost_algorithm.csv")
+
+submission.total_cases = np.concatenate([sj_predictions_gradient, iq_predictions_gradient])
+submission.to_csv("C:/Users/Dilshan/Documents/DengAI/data/benchmark_gradient_boosting_algorithm.csv")
+print("Done")
